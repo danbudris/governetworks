f36a984fa531e8f1f9191b77951415c989408614
dan.budris <d.c.budris@gmail.com>
2017-09-22 22:22:32 +0000

    initial commit

---
+++ api_functions.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var options = {
+  url: "https://api.propublica.org/congress/v1/115/senate/members.json",
+  headers: {
+    "X-API-Key": "yvle4Cp4cgvHZZLa1FGsah5XfxfpvkNcOx3WlZMy"
+  }
+};
+
+
+function callback(error, response, body){
+  if (!error && response.statusCode == 200){
+  var info = JSON.parse(body);
+  //console.log(info.results[0].members);
+  return info;
+  }
+};
+
+request(options, callback)
+
+
---
+++ app.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+MongoClient.connect('mongodb://172.17.0.2:27017/test', function(err, db) {
+    if(err) throw err;
+    
+    //create a new test collection, and write to it
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+        });
+    });
+    // locate all the entries using find
+    collection.find().toArray(function(err,results) {
+        console.dir(results);
+        
+        // close the db
+        db.close();
+    });
+});
