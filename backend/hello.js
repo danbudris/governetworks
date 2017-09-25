7db46196e4d1805efad01d270457c77677223a1d
danbudris <d.c.budris@gmail.com>
2017-09-25 00:14:06 +0000

    got hello.js to serve an html page!!

--- backend/hello.js
+++ backend/hello.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env nodejs
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, '0.0.0.0');
+var fs   = require('fs');
+
+fs.readFile('/srv/governetworks/frontend/projectProposal.html', function (err, html){
+    if (err) {
+        throw err;
+    } 
+    http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        res.end();
+    }).listen(8080, '0.0.0.0');
+});
 console.log('Server running at http://localhost:8080/');
