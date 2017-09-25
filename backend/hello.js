5bede3e8c43fd85d29803ebb229aa0b7f81de17a
danbudris <d.c.budris@gmail.com>
2017-09-25 00:16:48 +0000

    updated comments

--- backend/hello.js
+++ backend/hello.js
@@ -2,10 +2,13 @@
 var http = require('http');
 var fs   = require('fs');
 
+// Read in the file, then pass the results of that operation to the next function...
 fs.readFile('/srv/governetworks/frontend/projectProposal.html', function (err, html){
+    // if the previous read returns an error, throw that error; otherwise, proceed...
     if (err) {
         throw err;
     } 
+    // if no error, create an http server, and then write the html returned by the previous function, and serve it on 8080
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(html);
