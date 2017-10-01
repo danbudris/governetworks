1ab11a1540ddcee5dee14a0c7435c2f7c2758b74
danbudris <d.c.budris@gmail.com>
2017-10-01 00:26:02 +0000

    got it connecting ot the db and querying it

--- backend/serve.js
+++ backend/serve.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var pg = require('pg');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/frontend/projectproposal.html'));
+app.get('/senators', function(req, res) {
+    res.sendFile(path.join(__dirname + '/projectProposal.html'));
     });
 
+app.get('/test', function (req, res, next){
+
+  const { Pool, Client } = require('pg')
+  const conString = 'postgres://postgres:example@postgres/postgres'
+  const pool = new Pool({
+    connectionString: conString,
+  })
+  pool.query('SELECT * FROM public.congress_person', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  });
+});
 app.listen(8080);
