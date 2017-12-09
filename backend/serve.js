7b83141ed343b9764070ad4f33b83f919471a9ec
d.c.budris@gmail.com <d.c.budris@gmail.com>
2017-12-09 22:41:04 +0000

    updated html with path to static files; updated express with route to static files

--- backend/serve.js
+++ backend/serve.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('/static', express.static('public'));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/workspace', function(req, res) {
     res.sendFile(path.join(__dirname + '/../frontend/governet_workspace.html'));
