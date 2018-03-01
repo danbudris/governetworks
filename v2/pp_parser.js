d856befeae2a18c8ec9909c212032f5e371b3d26
Daniel Budris <d.c.budris@gmail.com>
2018-02-28 20:44:44 -0500

    Update pp_parser.js

--- v2/pp_parser.js
+++ v2/pp_parser.js
@@ -1,5 +1,5 @@
 //ProPublica Parser
-// This is a module for parsing the input of the ProPublica Congress API, and inserting the contents into the Postgres Database
+// This is a module for parsing the input of the ProPublica Congress API, and inserting the contents into the Mongo Database
 const request = require('request');
 var rp = require('request-promise');
 const MongoClient = require('mongodb').MongoClient;
