38f96f4a343ed14c1d25c17a3d8ab8eaba1e1489
Dan <d.c.budris@gmail.com>
2018-05-05 21:23:37 -0400

    removed more redundant files

--- v2/react/datatest.js
+++
@@ -1,7 +0,0 @@
-/*
-
-            "categories":[{"name":"Candidate","keyword:{},"base":"CandidateElement"},{"name":"Committee","keyword":{},"base":"CommitteeElement"}],
-            "nodes":[{"name":"","value":"","category":}]
-            "links":[{"source":0,"target":0}]
-
-*/
